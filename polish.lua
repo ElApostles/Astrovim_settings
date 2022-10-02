1d29b7d2c6466f82fde209a5566f9b14824e566b
Hyeokjin Doo <hdoo@c3r2s1.42seoul.kr>
2022-10-03 08:45:46 +0900

    change pattern

--- polish.lua
+++ polish.lua
@@ -1,12 +1,13 @@
 return function()
     vim.api.nvim_create_autocmd(
     { "BufRead", "BufNewFile", "BufEnter" },
-    { pattern = { "*.*pp", "*.h", }, command = "set tabstop=4" }
+    { pattern = { "*.hpp", "*.h", }, command = "set tabstop=4" }
     )
 
     -- FileType
     vim.filetype.add {
         extension = {
+
         },
         filename = {
             ["CmakeLists.txt"] = "cmake",
