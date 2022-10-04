6c4fe8e4033bf94bbcebce3ef1146461fe048796
Hyeokjin Doo <hdoo@c8r2s4.42seoul.kr>
2022-10-04 12:00:40 +0900

    Fix filetypedetection by update to nightly version

--- init.lua
+++ init.lua
@@ -1,4 +1,12 @@
 return {
+  updater = {
+    channel = "nightly",
+    branch = "fix-filetype-detection",
+    remote = "benvds",
+    remotes = {
+      ["benvds"] = "benvds/AstroNvim",
+    },
+  },
 	luasnip = {
     	vscode_snippet_paths = {
       		"./lua/user/snippets",
